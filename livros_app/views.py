4cc0006c7308c6c05b7aee394302fde72ce6376a
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-06-29 16:32:09 -0300

    Adicionando Interface usuario #7

--- livros_app/views.py
+++ livros_app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
+from .models import Livros
 from usuarios_app.models import Usuario
 
 def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
-        return HttpResponse(f'Olá, {usuario}')
-    else:
+        livros = Livros.objects.filter(usuario = usuario)
+        return render(request, 'home.html',{'livros': livros})
+    else: 
         return redirect('/auth/login/?status=2')
+
+def ver_livros(request, id):
+    livro = Livros.objects.get(id=id)
+    return render(request, 'ver_livro.html', {'livro': livro})
