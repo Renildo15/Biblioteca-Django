7a9a61af53aedcd066e5902b74b8fd2f7e0271cd
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-07-23 19:11:56 -0300

    Adicionando cadastro de categoria

--- livros_app/views.py
+++ livros_app/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Emprestimos, Livros, Categoria
 from usuarios_app.models import Usuario
-from .forms import CadastroLivro
+from .forms import CadastroLivro, CategoriaLivro
 
 def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
+        status_categoria = request.GET.get('cadastro_categoria')
         livros = Livros.objects.filter(usuario = usuario)
         form = CadastroLivro()
         form.fields['usuario'].initial = request.session['usuario']
         form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)
+        form_categoria = CategoriaLivro()
         return render(request, 'home.html',{'livros': livros, 'usuario_logado': request.session.get('usuario'), 
-        'form' : form})
+        'form' : form,'form_categoria': form_categoria, 'status_categoria': status_categoria})
     else: 
         return redirect('/auth/login/?status=2')
 
@@ -26,8 +28,10 @@ def ver_livros(request, id):
             usuario = Usuario.objects.get(id = request.session['usuario'])
             form.fields['usuario'].initial = request.session['usuario']
             form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)
+
+            form_categoria = CategoriaLivro()
             return render(request, 'ver_livro.html', {'livro': livro, 'categoria_livro': categoria_livro, 
-            'emprestimos' : emprestimos, 'usuario_logado': request.session.get('usuario'), 'form' : form})
+            'emprestimos' : emprestimos, 'usuario_logado': request.session.get('usuario'), 'form' : form, 'id_livro': id, 'form_categoria': form_categoria})
         else:
             return HttpResponse('Esse livro não é seu')
     return redirect('/auth/login/?status=2')
@@ -46,3 +50,23 @@ def cadastrar_livro(request):
             form = CadastroLivro()
             return render(request, 'home.html',{'livros': livros, 'usuario_logado': request.session.get('usuario'), 
             'form' : form})
+
+def excluir_livro(request, id):
+    livro = Livros.objects.get(id=id)
+    livro.delete()
+    return redirect('/livro/home')
+
+
+def cadastrar_categoria(request):
+    form = CategoriaLivro(request.POST)
+    nome = form.data['nome']
+    descricao = form.data['descricao']
+    id_usuario = request.POST.get('usuario')
+
+    if int(id_usuario) == int(request.session.get('usuario')):
+        user = Usuario.objects.get(id = id_usuario)
+        categoria = Categoria(nome=nome, descricao=descricao, usuario=user)
+        categoria.save()
+        return redirect('/livro/home?cadastro_categoria=1')
+    else:
+        return HttpResponse('Erro')
