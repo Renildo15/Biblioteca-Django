1fdb257baf98d22c3c2986c97159de259494267f
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-07-25 15:46:33 -0300

    Adicionando avaliação

--- livros_app/views.py
+++ livros_app/views.py
@@ -112,8 +112,39 @@ def devolver_livro(request):
     livro_devolver = Livros.objects.get(id = id)
     livro_devolver.emprestado = False
     livro_devolver.save()
-
+    
     emprestimo_devolver = Emprestimos.objects.get(Q(livro = livro_devolver) & Q(data_devolucao = None) )
-    emprestimo_devolver.data_devolucao = datetime.now()
-    emprestimo_devolver.save() 
+    emprestimo_devolver.data_devolucao = datetime.now() 
+    emprestimo_devolver.save()
+
     return redirect('/livro/home')
+
+def alterar_livro(request):
+    livro_id = request.POST.get('livro_id')
+    nome_livro = request.POST.get('nome_livro')
+    autor = request.POST.get('autor')
+    co_autor = request.POST.get('co_autor')
+    categoria_id = request.POST.get('categoria_id')
+
+    livro = Livros.objects.get(id = livro_id)
+    categoria = Categoria.objects.get(id = categoria_id)
+
+    if livro.usuario.id == request.session['usuario']:
+        livro.nome = nome_livro
+        livro.autor = autor
+        livro.co_autor = co_autor
+        livro.categoria = categoria
+        livro.save()
+        return redirect(f"/livro/ver_livro/{livro_id}")
+    else:
+        return redirect('/auth/sair')
+
+def seus_emprestimos(request):
+    usuario = Usuario.objects.get(id = request.session['usuario'])
+    emprestimos = Emprestimos.objects.filter(nome_emprestado = usuario)
+
+    return render(request, 'seus_emprestimos.html', {'usuario_logado': request.session['usuario'], 'emprestimos': emprestimos})
+
+
+def processa_avaliacao(request):
+    return HttpResponse('Teste')
